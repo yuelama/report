0c7b1d97a30196a6d573cbac68d57f7f794070ca
yuelama <55536932+yuelama@users.noreply.github.com>
2026-02-25 10:54:47 +0800

    增加了导航栏

---
+++ pages/home/home.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		<text>主页内容</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+		},
+		methods: {
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 6rpx;
+		height: 100vh;
+	}
+</style>
